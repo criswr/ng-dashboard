ba59cb31872fcff601ec944b3887c46c86489126
Cristian Yoji <cristianyoji@MacBook-Pro-de-Cristian.local>
2023-06-22 16:21:25 -0400

    add general layout, toolbar, sidebar, content

---
+++ src/app/general-layout/general-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-layout',
+  templateUrl: './general-layout.component.html',
+  styleUrls: ['./general-layout.component.css']
+})
+export class GeneralLayoutComponent {
+
+}
